5b047652fcf15b474647280e64b35804c6278475
Eunicekk <kate4234@gmail.com>
2025-09-24 20:23:08 +0900

    feat: style token 정의

---
+++ src/styles/tokens/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  primary: '#90DB28',
+  'primary-light': '#ADF14E',
+  'primary-dark': '##7BBE1E',
+  second: '#FAA60F',
+  'second-light': '#FFBD47',
+  'second-dark': '#DA910D',
+};
