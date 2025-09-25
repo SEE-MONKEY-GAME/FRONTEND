ffedbc51aa6e98d6f9da5456afc120bfb1e15e0b
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2025-09-25 18:47:09 +0900

    chore: style token 설정 및 적용 (#14)
    * feat: reset.css 적용
    
    * feat: style token 정의
    
    * feat: style token 적용 테스트

---
+++ src/styles/tokens/fonts.ts
@@ -0,0 +1,20 @@
+export const fonts = {
+  title: "'YeogiOttaeJalnan', sans-serif",
+  body: "'NanumSquare', sans-serif",
+};
+
+export const size = {
+  xs: '12px',
+  sm: '14px',
+  md: '16px',
+  lg: '20px',
+  xl: '24px',
+  '2xl': '32px',
+};
+
+export const weight = {
+  light: 300,
+  normal: 400,
+  bold: 700,
+  extraBold: 800,
+};
---
+++ src/styles/tokens/index.ts
@@ -0,0 +1,9 @@
+import { colors } from './colors';
+import { fonts, size, weight } from './fonts';
+
+export const theme = {
+  colors,
+  fonts,
+  size,
+  weight,
+};
