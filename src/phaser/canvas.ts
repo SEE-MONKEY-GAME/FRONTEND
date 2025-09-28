e108fc6026b6b83b83c0fa59bfe735acf2448e0e
Eunicekk <kate4234@gmail.com>
2025-09-28 18:23:53 +0900

    fix: 불필요한 주석 제거

--- src/phaser/canvas.ts
+++ src/phaser/canvas.ts
@@ -1,7 +1,6 @@
 import Phaser from 'phaser';
 
 const createGameCanvas = (parentId: string) => {
-  // 타입 어노테이션 사용
   const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     parent: parentId,
