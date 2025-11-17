dc025f215fffa2a92b98a055f0370da2ee0a00b2
Eunicekk <kate4234@gmail.com>
2025-11-17 18:31:41 +0900

    fix: 단위 수정

--- src/utils/formation-coin.ts
+++ src/utils/formation-coin.ts
@@ -1,12 +1,17 @@
 export const formatCoin = (value: number): string => {
+  if (value >= 1_000_000_000) {
+    const num = Math.floor((value / 1_000_000_000) * 100) / 100;
+    return `${num}B`;
+  }
+
   if (value >= 1_000_000) {
     const num = Math.floor((value / 1_000_000) * 100) / 100;
-    return `${num}m`;
+    return `${num}M`;
   }
 
   if (value >= 1_000) {
     const num = Math.floor((value / 1_000) * 100) / 100;
-    return `${num}k`;
+    return `${num}K`;
   }
 
   return value.toString();
